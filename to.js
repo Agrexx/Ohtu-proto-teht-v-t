8c5f247dd1c593b5582e02ef157c9f4329ae6ec0
Agrexx <61004074+Agrexx@users.noreply.github.com>
2020-09-22 09:13:59 +0300

    Add files via upload

---
+++ to.js
@@ -0,0 +1,24 @@
+function muunnaSalasanaksi()
+{
+  var pw = '';
+  var eka, toka;
+  pw = document.getElementById('salari').value;
+  alert(pw);
+  var taulu = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'x', 'y', 'z', 'å', 'ä', 'ö', 'w'];
+  var temp = [];
+  var lop='';
+  for(var s = 0; s < pw.length; s++)
+  {
+    eka = pw[s];
+    //alert[pw];
+    //alert[eka];
+    temp.push(eka);
+    //alert(eka);
+    var lisa = Math.floor(Math.random()*taulu.length);
+    toka = taulu[lisa];
+    temp.push(toka);
+    //alert(toka);
+  }
+  lop = temp.join('');
+  document.getElementById('salasana').innerHTML = lop;
+}
---
+++ to2.js
@@ -0,0 +1,31 @@
+function näytäParilliset()
+{
+  var nro1, nro2, palku, ralku, psumma=0, rsumma=0, pluvut='', rluvut='';
+  nro1 = parseInt(document.getElementById('pienempi').value);
+  nro2 = document.getElementById('isompi').value;
+  if(nro1%2 == 0)
+  {
+    palku = nro1;
+  }
+  else {
+    palku = nro1+1;
+  }
+  for(var b = palku; b<=nro2; b+=2)
+  {
+    pluvut += b + ' ';
+    psumma += b;
+  }
+  if(nro1%2 == 0)
+  {
+    ralku = nro1+1;
+  }
+  else {
+    ralku = nro1;
+  }
+  for(var r = ralku; r<=nro2; r+=2)
+  {
+    rluvut += r + ' ';
+    rsumma += r;
+  }
+  document.getElementById('pari').innerHTML = '<p>Parilliset numerot: ' + pluvut +' ja niiden summa: ' + psumma + '</p><p>Parittomat luvut: ' + rluvut + ' ja niiden summa: '+ rsumma + '</p>';
+}
