fb6a1eb225164569d405dc5a013fca9499cb1938
Agrexx <61004074+Agrexx@users.noreply.github.com>
2020-10-09 11:06:29 +0300

    Update ma2.js

--- ma2.js
+++ ma2.js
@@ -1,10 +1,10 @@
-function kertoma()
+function Kertoma()
 {
   var kerLuku = document.getElementById('kertoma').value;
   var laskettu = 1;
-for(var z = 1; z <= kerLuku; z++)
-{
-  laskettu*=z;
-}
-document.getElementById('tulostus').innerHTML = '<p>Luvun ' + kerLuku + ' kertoma on '+laskettu + '</p>';
+  for(var z = 1; z <= kerLuku; z++)
+  {
+    laskettu*=z;
+  }
+  document.getElementById('kertomaVastaus').innerHTML = '<p>Luvun ' + kerLuku + ' kertoma on '+laskettu + '</p>';
 }
